b1bf62da0e30ad403cf90ba0ec951e4b666a9ca2
Nick Lohmann <nicklohmann@outlook.de>
2024-09-13 12:45:00 +0200

    Ordering proper

--- Application/Models/Article.cs
+++ Application/Models/Article.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Application.Models;
 
 public class Article
@@ -5,6 +7,7 @@ public class Article
     public int Id { get; set; }
     public string Name { get; set; }
     public decimal Price { get; set; }
+    [ForeignKey("ArticleGroup")]
     public int ArticleGroupId { get; set; }
     public ArticleGroup ArticleGroup { get; set; }
 }
