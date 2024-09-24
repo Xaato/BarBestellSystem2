771fe30dbb43fc415ca601883a2e95808b618a97
FrydrycM <frydrycm@apetito.com>
2024-09-24 08:10:10 +0200

    updated migrations

--- Application/Models/Table.cs
+++ Application/Models/Table.cs
@@ -3,7 +3,7 @@
 public sealed class Table
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; } = string.Empty;
     public int X { get; set; }
     public int Y { get; set; }
 }
--- Application/Table.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Application;
-
-public sealed class Table
-{
-    public int Id { get; set; }
-    public string Name { get; set; } = string.Empty;
-    public int X { get; set; }
-    public int Y { get; set; }
-}
