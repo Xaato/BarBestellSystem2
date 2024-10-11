9250bdb7afea41b9e79f871d924e22ea80152b4a
Joshua Pott <pott.joshy@gmail.com>
2024-10-11 12:13:08 +0200

    Fix Logout

--- BarBestellSystem2/Pages/Logout.cshtml.cs
+++ BarBestellSystem2/Pages/Logout.cshtml.cs
@@ -1,37 +1,21 @@
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authentication;
 using Auth0.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
-using Microsoft.Extensions.Configuration;  
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace BarBestellSystem2.Pages
+namespace BarBestellSystem2.Pages;
+
+[Authorize]
+public class LogoutModel : PageModel
 {
-    [Authorize]
-    public class LogoutModel : PageModel
+    public async Task OnGet()
     {
-        private readonly IConfiguration _configuration;
+        var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
+            .WithRedirectUri("/Login")
+            .Build();
 
-        // Konstruktor, um die Konfiguration zu injizieren
-        public LogoutModel(IConfiguration configuration)
-        {
-            _configuration = configuration;
-        }
-
-        public async Task<IActionResult> OnGet(string redirectUri = "/Login")
-        {
-            // Auth0 Logout URL erstellen
-            var logoutUrl = $"https://{_configuration["Auth0:Domain"]}/v2/logout?client_id={_configuration["Auth0:ClientId"]}&returnTo={redirectUri}";
-
-            // Abmelden von Auth0
-            await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme);
-
-            // Abmelden von Cookie-Authentifizierung
-            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-
-            // Weiterleitung zur Auth0 Logout-URL
-            return Redirect(logoutUrl);
-        }
+        await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
     }
 }
