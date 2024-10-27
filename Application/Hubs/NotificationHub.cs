1053f2601bae2e7e4acfb45fbca5b14d7989e686
FrydrycM <misiu200105@web.de>
2024-10-27 19:58:26 +0100

    working stand of notifications

--- Application/Hubs/NotificationHub.cs
+++ Application/Hubs/NotificationHub.cs
@@ -1,11 +1,11 @@
 using Microsoft.AspNetCore.SignalR;
 
-namespace BarBestellSystem2.Hubs;
+namespace Application.Hubs;
 
 public class NotificationHub : Hub
 {
     public async Task SendNotification(string userId, string message)
     {
-        await Clients.User(userId).SendAsync("ReceiveNotification", message);
+        await Clients.All.SendAsync("ReceiveNotification", userId, message);
     }
 }
