d156a5cebf364ef9746b31a04b8bcdb2686f549c
FrydrycM <misiu200105@web.de>
2024-12-10 07:12:11 +0100

    save state rog

--- build_and_run.sh
+++ build_and_run.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 docker build -f Dockerfile -t barbestellsystem:latest .
-docker run -p 7292:8080 barbestellsystem:latest
+docker run -p 7292:80 barbestellsystem:latest
+
+docker pull mcr.microsoft.com/dotnet/samples:aspnetapp
+docker run --rm -it -p 8000:80 -p 8001:443 -e ASPNETCORE_URLS="https://+;http://+" -e ASPNETCORE_HTTPS_PORTS=8001 -e ASPNETCORE_Kestrel__Certificates__Default__Password="<CREDENTIAL_PLACEHOLDER>" -e ASPNETCORE_Kestrel__Certificates__Default__Path=/https/aspnetapp.pfx -v %USERPROFILE%\.aspnet\https:/https/ mcr.microsoft.com/dotnet/samples:aspnetapp
